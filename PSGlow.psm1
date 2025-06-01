b9be6da6bde8fc900b04d3b32ccc10088fe211e5
dfinke <finked@hotmail.com>
2025-06-01 09:43:37 -0400

    Refactor ANSI escape codes formatting and clean up Export-ModuleMember list

--- PSGlow.psm1
+++ PSGlow.psm1
@@ -3,37 +3,37 @@
 
 # ANSI escape codes for formatting
 $script:AnsiCodes = @{
-    Reset = "`e[0m"
-    Bold = "`e[1m"
-    Dim = "`e[2m"
-    Italic = "`e[3m"
-    Underline = "`e[4m"
+    Reset         = "`e[0m"
+    Bold          = "`e[1m"
+    Dim           = "`e[2m"
+    Italic        = "`e[3m"
+    Underline     = "`e[4m"
     
     # Colors
-    Black = "`e[30m"
-    Red = "`e[31m"
-    Green = "`e[32m"
-    Yellow = "`e[33m"
-    Blue = "`e[34m"
-    Magenta = "`e[35m"
-    Cyan = "`e[36m"
-    White = "`e[37m"
+    Black         = "`e[30m"
+    Red           = "`e[31m"
+    Green         = "`e[32m"
+    Yellow        = "`e[33m"
+    Blue          = "`e[34m"
+    Magenta       = "`e[35m"
+    Cyan          = "`e[36m"
+    White         = "`e[37m"
     
     # Bright colors
-    BrightBlack = "`e[90m"
-    BrightRed = "`e[91m"
-    BrightGreen = "`e[92m"
-    BrightYellow = "`e[93m"
-    BrightBlue = "`e[94m"
+    BrightBlack   = "`e[90m"
+    BrightRed     = "`e[91m"
+    BrightGreen   = "`e[92m"
+    BrightYellow  = "`e[93m"
+    BrightBlue    = "`e[94m"
     BrightMagenta = "`e[95m"
-    BrightCyan = "`e[96m"
-    BrightWhite = "`e[97m"
+    BrightCyan    = "`e[96m"
+    BrightWhite   = "`e[97m"
     
     # Background colors
-    BgDarkRed = "`e[48;5;52m"
+    BgDarkRed     = "`e[48;5;52m"
     
     # Salmon/coral color for parameters
-    Salmon = "`e[38;5;210m"
+    Salmon        = "`e[38;5;210m"
 }
 
 # Get all public function files
@@ -54,15 +54,5 @@ Set-Alias -Name 'psglow' -Value 'Invoke-PSGlow'
 
 # Export public functions and aliases
 Export-ModuleMember -Function @(
-    'Calculate-ColumnWidths'
-    'Format-CodeBlockLine'
-    'Format-InlineMarkdown'
-    'Format-MarkdownLine'
     'Invoke-PSGlow'
-    'Parse-TableAlignment'
-    'Render-Markdown'
-    'Render-Table'
-    'Render-TableRow'
-    'Split-TableCells'
-    'Test-TableLine'
 ) -Alias @('psglow')
